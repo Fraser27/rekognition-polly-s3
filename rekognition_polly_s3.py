b128839467efb672b4919d7a6643b814e02a56c8
Fraser Sequeira <leo27fraser@gmail.com>
2022-11-18 00:07:12 +0530

    Update rekognition_polly_s3.py
    Sample lambda

--- rekognition_polly_s3.py
+++ rekognition_polly_s3.py
@@ -18,7 +18,7 @@ rekognition = boto3.client('rekognition')
 s3 = boto3.client('s3')
 polly = boto3.client('polly')
 
-# --------------- Helper Functions to call Rekognition APIs ------------------
+# --------------- Rekognition APIs ------------------
 
 # Detect faces
 def detect_faces(image_bytes):
@@ -33,7 +33,7 @@ def detect_labels(image_bytes):
     print('Detected labels for image')    
     labels = []
     for label in response['Labels']:
-        if label['Confidence'] > 98:
+        if label['Confidence'] > 95:
             print (label['Name'] + ' : ' + str(label['Confidence']))
             labels.append(label['Name'])
     return labels
@@ -47,8 +47,8 @@ def search_face(image_bytes):
     Image={
         'Bytes': image_bytes
     },
-    MaxFaces=10,
-    FaceMatchThreshold=70,
+    MaxFaces=5,
+    FaceMatchThreshold=50,
     QualityFilter='AUTO')
     print("SearchFacesByImage :")
     for face in response['FaceMatches']:
@@ -65,6 +65,7 @@ def index_faces(event):
     msg = f"Welcome {event['face_name']} to our Shop"
     print(f'msg {msg}')
     try:
+        # When indexing a new face also store a welcome message to greet this user.
         response = polly.synthesize_speech(
                     Engine='standard',
                     Text=msg,
@@ -73,12 +74,9 @@ def index_faces(event):
                     VoiceId="Joanna")
                    
         with closing(response["AudioStream"]) as stream:
-            s3.put_object(Bucket='fs-ps-ns', Key=f"polly/welcome_audio_{event['face_name']}.mp3", Body=stream.read())
+            s3.put_object(Bucket='{your-bucket-name}', Key=f"polly/welcome_audio_{event['face_name']}.mp3", Body=stream.read())
     except BotoCoreError as error:
         print(error)
-    # upload it to S3
-    # Note: Collection has to be created upfront. Use CreateCollection API to create a collecion.
-    #rekognition.create_collection(CollectionId='PERSONALIZED_COLLECTION')
     image_bytes = base64.b64decode(event['store_face_img'])
     response = rekognition.index_faces(Image={"Bytes": image_bytes}, CollectionId="PERSONALIZED_COLLECTION", ExternalImageId=event['face_name'])
     return response
@@ -113,7 +111,7 @@ def rekog(event):
             if face_name is not None and face_name != '':
                 # Get a presigned s3 url of the audio message
                 url = s3.generate_presigned_url(ClientMethod='get_object',
-                                        Params={'Bucket': 'fs-ps-ns',
+                                        Params={'Bucket': '{your-bucket-name}',
                                                 'Key':f"polly/welcome_audio_{face_name}.mp3"
                                                },
                                         ExpiresIn=int(180)
@@ -125,16 +123,7 @@ def rekog(event):
                 response['labels'] = labels
         except Exception as e:
             print(e)
-        
-        
-
-        # Calls rekognition DetectLabels API to detect labels in S3 object
-        #response = detect_labels(bucket, key)
-
-        # Calls rekognition IndexFaces API to detect faces in S3 object and index faces into specified collection
-        #response = index_faces(bucket, key)
-            
-        # Print response to console.
+           
         print(response)
         
     except Exception as e:
